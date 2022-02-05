3ee7aa3aaabd181dad9fe3d5c42c32294dcaecb3
ManimaranArumugam <manimarankarthik231@gmail.com>
2022-02-05 19:14:10 +0530

    maran

---
+++ src/org/git/GitTask.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class GitTask {
+	private void add() {
+		System.out.println("add");	
+		
+	}
+	public static void main(String[] args) {
+		GitTask t = new GitTask();
+		t.add();
+	}
+
+}
